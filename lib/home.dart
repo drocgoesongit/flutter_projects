65876f2081848a16a6c9a9da9316c0852d1c0b29
drocgoesongit <82268112+drocgoesongit@users.noreply.github.com>
2022-08-01 20:41:02 +0530

    home page ui updated

--- lib/home.dart
+++ lib/home.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       home: const Scaffold(
+        backgroundColor: Color(0xFFEAF4FF),
         body: MainScreenBody(),
       ),
       routes: {
@@ -44,13 +45,14 @@ class MainScreenBody extends StatelessWidget {
           SizedBox(
             height: 180.0,
             width: MediaQuery. of(context). size. width,
-            child: Row(
+            child: Row( // top row for title and title image.
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Padding(
                   padding: const EdgeInsets.all(30.0),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: const [
                       Text("Practice", style: kExtraBoldTitleStyle,),
                       Text("Apps", style: kExtraBoldTitleStyle,),
@@ -64,87 +66,79 @@ class MainScreenBody extends StatelessWidget {
               ]
             ),
           ),
+          Expanded(
+            child: ListView(
+                children: [
+                  TileElementApplicationList(route: '/bitcoin', appContext: 'Bitcoin - Convert your crypto in your local currency', thumbnailAsset: 'assets/images/bitcoin_bg.png'),
+                  const SizedBox(
+                      height: 20.0,
+                  ),
+                  TileElementApplicationList(route: '/music', appContext: 'Xylo - Uses sound packages to play some xylophone tunes.', thumbnailAsset: 'assets/images/music_xylo_bg.png'),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  TileElementApplicationList(route: '/clima', appContext: 'Clima - Check about climate of your city in no time', thumbnailAsset: 'assets/images/clima_app_bg.png'),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  TileElementApplicationList(route: '/bmi', appContext: 'BMI - Check your body status by calculating BMI instantly.', thumbnailAsset: 'assets/images/bmi_bg.png'),
+                ],
+            ),
+          ),
 
-          SingleChildScrollView(
-            child: GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, '/bitcoin');
-              },
+        ],
+      ),
+    );
+  }
+}
+
+// tile element for each application.
+class TileElementApplicationList extends StatelessWidget {
+  TileElementApplicationList({required this.route, required this.appContext, required this.thumbnailAsset });
+
+  final String route;
+  final String appContext;
+  final String thumbnailAsset;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, route);
+      },
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: kNotWhite,
+                borderRadius: BorderRadius.circular(20.0),
+              ),
               child: Column(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        color: kNotWhite,
-                      ),
-                      child: Column(
-                        children: [
-                          SizedBox(
-
-                              child: Image.asset('assets/images/bitcoin_bg.png',
-                          fit: BoxFit.contain,)),
-                          Row(
-                            children: const [
-                              Expanded(
-                                  child: Padding(
-                                    padding: EdgeInsets.all(15.0),
-                                    child: Text("Bitcoin - Convert your crypto in your local currency", style: kCardTextHomePage),
-                                  )),
-                              Padding(
-                                padding: EdgeInsets.only(right: 15.0),
-                                child: Icon(
-                                  Icons.arrow_forward_ios,
-                                ),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
+                  SizedBox(
+                      child: Image.asset(thumbnailAsset,
+                        fit: BoxFit.contain,)),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Text(appContext, style: kCardTextHomePage),
+                          )),
+                      const Padding(
+                        padding: EdgeInsets.only(right: 15.0),
+                        child: Icon(
+                          Icons.arrow_forward_ios,
+                        ),
+                      )
+                    ],
                   )
                 ],
               ),
             ),
-          ),
-
-          MaterialButton(
-              onPressed: (){
-                Navigator.pushNamed(context, '/bmi');
-              },
-            child: const Text("BMI Calculator"),
-            ),
-          const SizedBox(
-            height: 10.0,
-            width: double.infinity,
-          ),
-          MaterialButton(
-            onPressed: (){
-              Navigator.pushNamed(context, '/music');
-            },
-            child: const Text("Button2"),
-          ),
-          const SizedBox(
-            height: 10.0,
-            width: double.infinity,
-          ),
-          MaterialButton(
-            onPressed: (){
-              Navigator.pushNamed(context, '/clima');
-            },
-            child: const Text("Clima app"),
-          ),
-          const SizedBox(
-            height: 10.0,
-            width: double.infinity,
-          ),
-          MaterialButton(
-            onPressed: (){
-              Navigator.pushNamed(context, '/bitcoin');
-            },
-            child: const Text("Bitcoin app"),
-          ),
-
+          )
         ],
       ),
     );
---
+++ lib/todo/task.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Task {
+  Task({required this.task, required this.isDone});
+
+  String task;
+  bool isDone;
+
+}
